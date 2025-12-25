18c4aa71e202e91aa18d5c7885e6d3cd241f885a
boub40357 <boub40357@gmail.com>
2025-12-25 10:15:09 +0000

    Bolt

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,31 +8,44 @@
  * @module
  */
 
+import type * as user from "../user.js";
+import type * as workspace from "../workspace.js";
+
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as user from "../user.js";
-import type * as workspace from "../workspace.js";
+
+declare const fullApi: ApiFromModules<{
+  user: typeof user;
+  workspace: typeof workspace;
+}>;
 
 /**
- * A utility for referencing Convex functions in your app's API.
+ * A utility for referencing Convex functions in your app's public API.
  *
  * Usage:
  * ```js
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{
-  user: typeof user;
-  workspace: typeof workspace;
-}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
 >;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
 export declare const internal: FilterApi<
   typeof fullApi,
   FunctionReference<any, "internal">
 >;
+
+export declare const components: {};
